317b85e7fea426ef19ded2b8301881a50db9ee14
Ahmed Wefky <awefky@icloud.com>
2025-12-24 18:46:11 +0100

    Added the default buildroot configuration and fixed config files

--- base_external/external.mk
+++ base_external/external.mk
@@ -1,2 +1,4 @@
 # Register the external path so Buildroot can find package .mk files
-BR2_EXTERNAL_PROJECT_BASE_PATH := $(dir $(lastword $(MAKEFILE_LIST)))
+BR2_EXTERNAL_PROJECT_BASE_PATH = $(BR2_EXTERNAL)
+
+include $(sort $(wildcard $(BR2_EXTERNAL_PROJECT_BASE_PATH)/package/*/*.mk))
--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-AESD_ASSIGNMENTS_VERSION = 0bddb356e7c62f64272c59adff1c488baccb4484
+AESD_ASSIGNMENTS_VERSION = 7793f766d434d5eaefb16635055e56591bf52677
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -22,7 +22,10 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -d 0755 $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/finder-app/* $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin
+	$(INSTALL) -d 0755 $(TARGET_DIR)/bin
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
 endef
 
